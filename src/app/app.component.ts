aa75b0933e0ba0f68d5226a03ae3733b46b5c246
ADI <adinh@jiway.lu>
2023-05-06 16:35:38 +0200

    app.component.ts : testing HHTP request when initiating component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'eShopping-frontend';
+export class AppComponent implements OnInit {
+  title = 'eShopping';
+
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void {
+
+    this.http.get('http://localhost:9010/Catalog/GetProductsByBrandName/Adidas').subscribe(
+      {
+        next: response => console.log(response),
+        error: error => console.log(error),
+        complete: () => console.log('Catalog API call completed')
+      }
+    )
+  }
 }
