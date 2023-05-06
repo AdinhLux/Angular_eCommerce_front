f89ce20f0c81c92402db31da7dd25305e1eccc2e
ADI <adinh@jiway.lu>
2023-05-06 16:59:46 +0200

    Testing models

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {IProduct} from "./shared/models/product";
+import {IPagination} from "./shared/models/pagination";
 
 @Component({
   selector: 'app-root',
@@ -8,17 +10,17 @@ import {HttpClient} from "@angular/common/http";
 })
 export class AppComponent implements OnInit {
   title = 'eShopping';
-  products: any[] = [];
+  products: IProduct[] = [];
 
   constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void {
 
-    this.http.get('http://localhost:9010/Catalog/GetProductsByBrandName/Adidas').subscribe(
+    this.http.get<IPagination<IProduct[]>>('http://localhost:9010/Catalog/GetAllProducts').subscribe(
       {
-        next: (response: any) => {
-          this.products = response;
+        next: (response) => {
+          this.products = response.data;
           console.log(response);
         },
         error: error => console.log(error),
