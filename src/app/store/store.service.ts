5f8b6537b146600efb3c11d4feb1428c09a6d7b4
ADI <adinh@jiway.lu>
2023-05-07 18:44:50 +0200

    NEW Brand & Type models + implementing in services

--- src/app/store/store.service.ts
+++ src/app/store/store.service.ts
@@ -2,6 +2,8 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {IPagination} from "../shared/models/pagination";
 import {IProduct} from "../shared/models/product";
+import {IBrand} from "../shared/models/brand";
+import {IType} from "../shared/models/type";
 
 @Injectable({
   providedIn: 'root' // 'root' is app-module.ts
@@ -15,5 +17,13 @@ export class StoreService {
 
   getProducts() {
     return this.http.get<IPagination<IProduct[]>>(this.baseUrl + 'Catalog/GetAllProducts')
+  }
+
+  getBrands() {
+    return this.http.get<IBrand[]>(this.baseUrl + 'Catalog/GetAllBrands');
+  }
+
+  getTypes() {
+    return this.http.get<IType[]>(this.baseUrl + 'Catalog/GetAllTypes');
   }
 }
