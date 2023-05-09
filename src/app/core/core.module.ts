2f225c4e85f208daa8de4282160028c8d68e731b
ADI <adinh@jiway.lu>
2023-05-09 13:32:44 +0200

    ng g c core/server-error --skip-tests

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,13 +4,15 @@ import {NavbarComponent} from "./navbar/navbar.component";
 import {RouterModule} from "@angular/router";
 import { NotFoundComponent } from './not-found/not-found.component';
 import { UnAuthenticatedComponent } from './un-authenticated/un-authenticated.component';
+import { ServerErrorComponent } from './server-error/server-error.component';
 
 
 @NgModule({
   declarations: [
     NavbarComponent,
     NotFoundComponent,
-    UnAuthenticatedComponent
+    UnAuthenticatedComponent,
+    ServerErrorComponent
   ],
   imports: [
     CommonModule,
