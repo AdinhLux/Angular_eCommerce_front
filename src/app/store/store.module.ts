605fedcc2dedf3e819717ebd5e3812df7acd11ef
ADI <adinh@jiway.lu>
2023-05-09 09:31:00 +0200

    ng g c store/product-details --skip-tests

--- src/app/store/store.module.ts
+++ src/app/store/store.module.ts
@@ -3,7 +3,7 @@ import {CommonModule} from '@angular/common';
 import {StoreComponent} from './store.component';
 import {ProductItemsComponent} from './product-items/product-items.component';
 import {SharedModule} from "../shared/shared.module";
-import { ProductDetailsComponent } from './product-details.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 
 @NgModule({
