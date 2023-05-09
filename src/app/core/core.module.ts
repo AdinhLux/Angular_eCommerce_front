f9f2e84a2f25eb7d44ddbf2710ab3a84cad4b422
ADI <adinh@jiway.lu>
2023-05-09 13:28:55 +0200

    ng g c core/not-found --skip-tests

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NavbarComponent} from "./navbar/navbar.component";
 import {RouterModule} from "@angular/router";
+import { NotFoundComponent } from './not-found/not-found.component';
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    NotFoundComponent
   ],
   imports: [
     CommonModule,
