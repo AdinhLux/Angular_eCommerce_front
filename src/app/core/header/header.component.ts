a0c0e330dfad35087c2c13df60eb9f49a0bdca76
ADI <adinh@jiway.lu>
2023-05-09 21:21:18 +0200

    Implement Home Page

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,5 @@
 import {Component} from '@angular/core';
+import {BreadcrumbService} from "xng-breadcrumb";
 
 @Component({
   selector: 'app-header',
@@ -6,4 +7,6 @@ import {Component} from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  constructor(public bcService: BreadcrumbService) {
+  }
 }
