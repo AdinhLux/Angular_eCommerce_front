bfa6c034061b10f7d94160d38e12156b1d20faff
ADI <adinh@jiway.lu>
2023-05-09 21:12:43 +0200

    Loading spinner implementation part 2

--- src/app/core/interceptors/loading.interceptor.ts
+++ src/app/core/interceptors/loading.interceptor.ts
@@ -1,18 +1,21 @@
-import { Injectable } from '@angular/core';
-import {
-  HttpRequest,
-  HttpHandler,
-  HttpEvent,
-  HttpInterceptor
-} from '@angular/common/http';
-import { Observable } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {delay, finalize, Observable} from 'rxjs';
+import {LoadingService} from "../services/loading.service";
 
 @Injectable()
 export class LoadingInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor(private loadingService: LoadingService) {
+  }
 
-  intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-    return next.handle(request);
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.loadingService.loading();
+    return next.handle(request).pipe(
+      delay(1000),
+      finalize(() => {
+        this.loadingService.idle();
+      })
+    );
   }
 }
