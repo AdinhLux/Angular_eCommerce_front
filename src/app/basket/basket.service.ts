067621f34d592a3f35526166ef563f21a39e08de
ADI <adinh@jiway.lu>
2023-05-10 21:47:09 +0200

    basket.service.ts : implementation part 1

--- src/app/basket/basket.service.ts
+++ src/app/basket/basket.service.ts
@@ -1,9 +1,39 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {BehaviorSubject} from "rxjs";
+import {Basket, IBasket} from "../shared/models/basket";
 
 @Injectable({
   providedIn: 'root'
 })
 export class BasketService {
+  baseUrl = 'https://localhost:9010';
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+  }
+
+  // Our dynamic variable, when we add or remove items. Default value is NULL
+  private basketSource = new BehaviorSubject<Basket | null>(null);
+  basketSource$ = this.basketSource.asObservable();
+
+  getBasket(username: string) {
+    return this.http.get<IBasket>(this.baseUrl + '/Basket/GetBasket/rahul').subscribe({
+      // Update the basketSource so that via observable these values will be available to the subscribers via component
+      next: basket => {
+        this.basketSource.next(basket);
+      }
+    });
+  }
+
+  setBasket(basket: IBasket) {
+    return this.http.post<IBasket>(this.baseUrl + '/Basket/CreateBasket', basket).subscribe({
+      next: basket => {
+        this.basketSource.next(basket);
+      }
+    });
+  }
+
+  getCurrentBasket() {
+    return this.basketSource.value;
+  }
 }
