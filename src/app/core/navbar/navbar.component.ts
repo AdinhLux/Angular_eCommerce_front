8b05ce58345d5c776c35144538e3e6d1a13d6d2c
ADI <adinh@jiway.lu>
2023-05-11 22:33:20 +0200

    Count total items in basket

--- src/app/core/navbar/navbar.component.ts
+++ src/app/core/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {BasketService} from "../../basket/basket.service";
+import {IBasketItem} from "../../shared/models/basket";
 
 @Component({
   selector: 'app-navbar',
@@ -10,4 +11,7 @@ export class NavbarComponent {
   constructor(public basketService: BasketService) {
   }
 
+  getBasketCount(items: IBasketItem[]) {
+    return items.reduce((sum, item) => sum + item.quantity, 0);
+  }
 }
