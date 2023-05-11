bbe49b4d93103b5c23c45ca0129274f0548a1e12
ADI <adinh@jiway.lu>
2023-05-11 21:33:35 +0200

    product-items.component : addItemToBasket()

--- src/app/store/product-items/product-items.component.ts
+++ src/app/store/product-items/product-items.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {IProduct} from "../../shared/models/product";
+import {BasketService} from "../../basket/basket.service";
 
 @Component({
   selector: 'app-product-items',
@@ -8,4 +9,12 @@ import {IProduct} from "../../shared/models/product";
 })
 export class ProductItemsComponent {
   @Input() product?: IProduct; // ? means 'product' is nullable
+
+  constructor(private basketService: BasketService) {
+  }
+
+  addItemToBasket() {
+    // This kind of coding means we can execute the method if there is a product
+    this.product && this.basketService.addItemToBasket(this.product);
+  }
 }
